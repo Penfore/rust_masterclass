bc6f92baed69af5d73cf31d866ecdc4bfff52e6e
Fúlvio Leo <fulviokaldas@outlook.com>
2024-04-09 22:20:56 -0300

    adicionado exemplos de funções

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod utils;
 mod fundamentos;
 mod tipos;
 mod controle;
+mod funcoes;
 
 use std::process::exit;
 use utils::terminal::{exibir_menu, limpar_tela};
@@ -24,7 +25,7 @@ fn main() {
             1 => fundamentos::executar(),
             2 => tipos::executar(),
             3 => controle::executar(),
-            4 => println!("4"),
+            4 => funcoes::executar(),
             5 => println!("5"),
             _ => exit(0),
         }
